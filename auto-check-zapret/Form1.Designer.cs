883d3e5df810920537cbc4b369ae39876dcb4d7c
Alexander Kokorin <kaciel13@yahoo.com>
2025-11-24 14:45:11 +0600

    Изменен дизайн подобран новый шрифт и изменена палитра цветов.

--- auto-check-zapret/Form1.Designer.cs
+++ auto-check-zapret/Form1.Designer.cs
@@ -52,14 +52,14 @@
             mainTableLayout.Controls.Add(checktTableLayout, 0, 3);
             mainTableLayout.Dock = DockStyle.Fill;
             mainTableLayout.Location = new Point(0, 0);
-            mainTableLayout.Margin = new Padding(3, 4, 3, 4);
+            mainTableLayout.Margin = new Padding(4, 3, 4, 3);
             mainTableLayout.Name = "mainTableLayout";
             mainTableLayout.RowCount = 4;
-            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 28.0575542F));
-            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 71.9424438F));
-            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 39F));
-            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 456F));
-            mainTableLayout.Size = new Size(551, 671);
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 12.2807016F));
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 87.7193F));
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 93F));
+            mainTableLayout.Size = new Size(758, 470);
             mainTableLayout.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -70,83 +70,105 @@
             tableLayoutPanel1.Controls.Add(choiceVersionComboBox, 0, 0);
             tableLayoutPanel1.Controls.Add(downloadButton, 1, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(3, 4);
-            tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
+            tableLayoutPanel1.Location = new Point(4, 3);
+            tableLayoutPanel1.Margin = new Padding(4, 3, 4, 3);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(545, 41);
+            tableLayoutPanel1.Size = new Size(750, 36);
             tableLayoutPanel1.TabIndex = 1;
             // 
             // choiceVersionComboBox
             // 
+            choiceVersionComboBox.BackColor = Color.FromArgb(30, 30, 30);
             choiceVersionComboBox.Dock = DockStyle.Fill;
-            choiceVersionComboBox.Font = new Font("Unispace", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            choiceVersionComboBox.DropDownHeight = 300;
+            choiceVersionComboBox.DropDownWidth = 357;
+            choiceVersionComboBox.FlatStyle = FlatStyle.System;
+            choiceVersionComboBox.Font = new Font("LCD5x8H", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            choiceVersionComboBox.ForeColor = SystemColors.Window;
             choiceVersionComboBox.FormattingEnabled = true;
-            choiceVersionComboBox.Location = new Point(3, 4);
-            choiceVersionComboBox.Margin = new Padding(3, 4, 3, 4);
+            choiceVersionComboBox.IntegralHeight = false;
+            choiceVersionComboBox.ItemHeight = 28;
+            choiceVersionComboBox.Location = new Point(0, 0);
+            choiceVersionComboBox.Margin = new Padding(0);
+            choiceVersionComboBox.MaxDropDownItems = 10;
             choiceVersionComboBox.Name = "choiceVersionComboBox";
-            choiceVersionComboBox.Size = new Size(357, 32);
+            choiceVersionComboBox.Size = new Size(500, 36);
             choiceVersionComboBox.TabIndex = 0;
             choiceVersionComboBox.SelectedIndexChanged += choiceVersionComboBox_SelectedIndexChanged;
             // 
             // downloadButton
             // 
+            downloadButton.BackColor = Color.FromArgb(64, 64, 64);
             downloadButton.Dock = DockStyle.Fill;
             downloadButton.Enabled = false;
-            downloadButton.Location = new Point(366, 4);
-            downloadButton.Margin = new Padding(3, 4, 3, 4);
+            downloadButton.FlatAppearance.BorderSize = 0;
+            downloadButton.FlatStyle = FlatStyle.Flat;
+            downloadButton.Font = new Font("LCD5x8H", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            downloadButton.ForeColor = Color.Silver;
+            downloadButton.Location = new Point(504, 3);
+            downloadButton.Margin = new Padding(4, 3, 4, 3);
             downloadButton.Name = "downloadButton";
-            downloadButton.Size = new Size(176, 33);
+            downloadButton.Size = new Size(242, 30);
             downloadButton.TabIndex = 1;
             downloadButton.Text = "Не выбрана версия";
-            downloadButton.UseVisualStyleBackColor = true;
+            downloadButton.UseVisualStyleBackColor = false;
             downloadButton.Click += downloadButton_Click;
             // 
             // infoTextBox
             // 
+            infoTextBox.BackColor = SystemColors.ActiveCaptionText;
+            infoTextBox.BorderStyle = BorderStyle.None;
             infoTextBox.Dock = DockStyle.Fill;
-            infoTextBox.Location = new Point(3, 53);
-            infoTextBox.Margin = new Padding(3, 4, 3, 4);
+            infoTextBox.Font = new Font("LCD5x8H", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            infoTextBox.ForeColor = SystemColors.ControlLightLight;
+            infoTextBox.Location = new Point(4, 45);
+            infoTextBox.Margin = new Padding(4, 3, 4, 3);
             infoTextBox.Multiline = true;
             infoTextBox.Name = "infoTextBox";
             infoTextBox.ReadOnly = true;
             infoTextBox.ScrollBars = ScrollBars.Vertical;
-            infoTextBox.Size = new Size(545, 118);
+            infoTextBox.Size = new Size(750, 294);
             infoTextBox.TabIndex = 2;
             // 
             // progressBar
             // 
+            progressBar.BackColor = Color.Black;
             progressBar.Dock = DockStyle.Fill;
-            progressBar.Location = new Point(3, 179);
-            progressBar.Margin = new Padding(3, 4, 3, 4);
+            progressBar.ForeColor = Color.White;
+            progressBar.Location = new Point(4, 345);
+            progressBar.Margin = new Padding(4, 3, 4, 3);
             progressBar.Name = "progressBar";
-            progressBar.Size = new Size(545, 31);
+            progressBar.Size = new Size(750, 28);
+            progressBar.Style = ProgressBarStyle.Continuous;
             progressBar.TabIndex = 3;
             // 
             // checktTableLayout
             // 
+            checktTableLayout.BackColor = Color.FromArgb(25, 25, 25);
             checktTableLayout.ColumnCount = 2;
             checktTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             checktTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             checktTableLayout.Controls.Add(button1, 0, 0);
             checktTableLayout.Dock = DockStyle.Fill;
-            checktTableLayout.Location = new Point(3, 218);
-            checktTableLayout.Margin = new Padding(3, 4, 3, 4);
+            checktTableLayout.Location = new Point(4, 379);
+            checktTableLayout.Margin = new Padding(4, 3, 4, 3);
             checktTableLayout.Name = "checktTableLayout";
             checktTableLayout.RowCount = 2;
             checktTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             checktTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            checktTableLayout.Size = new Size(545, 449);
+            checktTableLayout.Size = new Size(750, 88);
             checktTableLayout.TabIndex = 4;
             // 
             // button1
             // 
-            button1.Location = new Point(3, 4);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(4, 3);
+            button1.Margin = new Padding(4, 3, 4, 3);
             button1.Name = "button1";
-            button1.Size = new Size(266, 49);
+            button1.Size = new Size(367, 38);
             button1.TabIndex = 0;
             button1.Text = "Проверить все обходы";
             button1.UseVisualStyleBackColor = true;
@@ -154,13 +176,15 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(11F, 14F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(551, 671);
+            BackColor = Color.FromArgb(10, 10, 10);
+            ClientSize = new Size(758, 470);
             Controls.Add(mainTableLayout);
-            Margin = new Padding(3, 4, 3, 4);
-            MaximumSize = new Size(569, 718);
-            MinimumSize = new Size(569, 718);
+            Font = new Font("LCD5x8H", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4, 3, 4, 3);
+            MaximumSize = new Size(776, 517);
+            MinimumSize = new Size(776, 517);
             Name = "Form1";
             Text = "AutoCheckZapret";
             mainTableLayout.ResumeLayout(false);
