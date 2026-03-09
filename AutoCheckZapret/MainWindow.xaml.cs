5af0d487ec60852ddf2c7e40057c52477d5d77f8
kaciel13 <kaciel13@yahoo.com>
2026-03-09 16:08:28 +0600

    Сделал основаные методы для управления окном

--- AutoCheckZapret/MainWindow.xaml.cs
+++ AutoCheckZapret/MainWindow.xaml.cs
@@ -11,7 +11,8 @@ namespace AutoCheckZapret
     public partial class MainWindow : Window
     {
         private readonly ZapretDownloaderService _downloaderService;
-
+        private readonly WindowStateService _windowStateService;
+   
         /// <summary>
         /// Конструктор главного окна приложения
         /// </summary>
@@ -22,13 +23,14 @@ namespace AutoCheckZapret
             Assembly assembly = Assembly.GetExecutingAssembly();
             AssemblyName assemblyName = assembly.GetName();
             Version version = assemblyName.Version!;
-
+            
             // В конце используем Build, потому что в .csproj используем вид Major.Minor.Feature, а не Major.Minor.Feature.Build
             // А VS определяет последнюю цифру как Build
-            Title = $"Auto Check Zapret v{version.Major}.{version.Minor}.{version.Build}";
+            lbTitle.Content = $"Auto Check Zapret v{version.Major}.{version.Minor}.{version.Build}";
 
             _downloaderService = new ZapretDownloaderService();
-
+            _windowStateService = new WindowStateService(this); // Передаем главное MainWindow для перетаскивания
+ 
             FillZapretVersionsComboBox();
         }
 
@@ -48,6 +50,26 @@ namespace AutoCheckZapret
             // Эти данные будут сохраняться в файлике рядом с прогой ACZ
             ZapretVersionsComboBox.SelectedIndex = 0;
             ZapretVersionsComboBox.IsEnabled = true;
+        }
+
+        private void btnCloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            _windowStateService.ShutDownApplication();
+        }
+
+        private void btnMinimizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            _windowStateService.MinimizeWindow();
+        }
+
+        private void btnToggleFullscreen_Click(object sender, RoutedEventArgs e)
+        {
+            _windowStateService.ToggleFullscreen();
+        }
+
+        private void DragWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _windowStateService.DragWindow();
         }
     }
 }
---
+++ AutoCheckZapret/Services/WindowStateService.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace AutoCheckZapret.Services
+{
+    /// <summary>
+    /// Класс для управления состоянием окна приложения.
+    /// </summary>
+    public class WindowStateService
+    {
+        private WindowState _windowState = Application.Current.MainWindow.WindowState;
+        private MainWindow _mainWindow;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public WindowStateService(MainWindow window)
+        {
+            // Начальные настройки можно сделать здесь
+            Application.Current.MainWindow.WindowState = _windowState;
+            _mainWindow = window;
+        }
+
+        /// <summary>
+        /// Завершает работу приложения.
+        /// </summary>
+        public void ShutDownApplication()
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Сворачивает текущее окно.
+        /// </summary>
+        public void MinimizeWindow()
+        {
+            _windowState = WindowState.Minimized;
+            ChangeState(_windowState);
+        }
+
+        /// <summary>
+        /// Переключает состояние окна между полноэкранным и обычным режимами.
+        /// </summary>
+        public void ToggleFullscreen()
+        {
+            if (_windowState == WindowState.Normal)
+            {
+                _windowState = WindowState.Maximized;
+                ChangeState(_windowState);
+            }
+            else
+            {
+                _windowState = WindowState.Normal;
+                ChangeState(_windowState);
+            }
+        }
+
+        /// <summary>
+        /// Перетаскивает окно при нажатой левой кнопке мыши.
+        /// </summary>
+        public void DragWindow()
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                // Запускаем перемещение окна
+                _mainWindow.DragMove();
+            }
+        }
+
+        /// <summary>
+        /// Изменяет состояние главного окна приложения.
+        /// </summary>
+        /// <param name="windowState">Новое состояние окна.</param>
+        private void ChangeState(WindowState windowState)
+        {
+            Application.Current.MainWindow.WindowState = windowState;
+        }
+    }
+}
