83b092956bb1830a4ed0649ad6ac16eb8c69192c
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2026-03-09 12:33:00 +0600

    Реализовано получение доступных версий Zapret при запуске программы

--- AutoCheckZapret/MainWindow.xaml.cs
+++ AutoCheckZapret/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using AutoCheckZapret.Models;
+using AutoCheckZapret.Services;
+using System.Reflection;
 using System.Windows;
 
 namespace AutoCheckZapret
@@ -8,22 +10,44 @@ namespace AutoCheckZapret
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ZapretDownloaderService _downloaderService;
+
+        /// <summary>
+        /// Конструктор главного окна приложения
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
 
-            // Get the Assembly object for the currently executing assembly
             Assembly assembly = Assembly.GetExecutingAssembly();
-
-            // Get the AssemblyName object
             AssemblyName assemblyName = assembly.GetName();
-
-            // Get the Version object
             Version version = assemblyName.Version!;
 
             // В конце используем Build, потому что в .csproj используем вид Major.Minor.Feature, а не Major.Minor.Feature.Build
             // А VS определяет последнюю цифру как Build
-            this.Title = $"Auto Check Zapret v{version.Major}.{version.Minor}.{version.Build}";
+            Title = $"Auto Check Zapret v{version.Major}.{version.Minor}.{version.Build}";
+
+            _downloaderService = new ZapretDownloaderService();
+
+            FillZapretVersionsComboBox();
+        }
+
+        private async void FillZapretVersionsComboBox()
+        {
+            List<ZapretVersion> zapretVersions = await _downloaderService.FetchAvailableVersions();
+
+            ZapretVersionsComboBox.IsEnabled = false;
+            ZapretVersionsComboBox.Items.Clear();
+
+            foreach (ZapretVersion version in zapretVersions)
+            {
+                ZapretVersionsComboBox.Items.Add(version.Number);
+            }
+
+            // TODO: В дальнейшем нужно будет устанавливать индекс в зависимости от того, какую версию пользователь сам себе ставил в последний раз.
+            // Эти данные будут сохраняться в файлике рядом с прогой ACZ
+            ZapretVersionsComboBox.SelectedIndex = 0;
+            ZapretVersionsComboBox.IsEnabled = true;
         }
     }
 }
