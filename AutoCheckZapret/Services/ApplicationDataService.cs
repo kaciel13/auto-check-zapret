d78a6c59da89919e51cb80562f1b8a45528e072f
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2026-03-10 12:24:11 +0600

    Добавлены классы для работы с данными приложения (настройками пользователя)

---
+++ AutoCheckZapret/Services/ApplicationDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCheckZapret.Services
+{
+    /// <summary>
+    /// Сервис для сохранения/загрузки данных приложения в файл сохранения
+    /// </summary>
+    public class ApplicationDataService
+    {
+    }
+}
