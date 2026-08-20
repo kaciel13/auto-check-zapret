e5a90f400e2810a2c937a1d3920f7455e09b440b
Лубенский Игорь <i.lubensky@mcad.ru>
2026-08-20 11:30:54 +0600

    style Убрал неиспользуемые using

--- AutoCheckZapret/Models/ApplicationData.cs
+++ AutoCheckZapret/Models/ApplicationData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AutoCheckZapret.Models
+﻿namespace AutoCheckZapret.Models
 {
     /// <summary>
     /// Данные приложения (настройки, выбранные версии Zapret) и другое, которые
