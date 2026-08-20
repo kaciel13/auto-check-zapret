e6ecd271abbc8b5f632f3a9dac755bca75679141
Лубенский Игорь <i.lubensky@mcad.ru>
2026-08-20 11:31:47 +0600

    style Убрал неиспользуемые using

--- AutoCheckZapret/Services/ApplicationDataService.cs
+++ AutoCheckZapret/Services/ApplicationDataService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AutoCheckZapret.Services
+﻿namespace AutoCheckZapret.Services
 {
     /// <summary>
     /// Сервис для сохранения/загрузки данных приложения в файл сохранения
