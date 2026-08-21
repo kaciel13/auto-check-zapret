dd331f63387396ec8cbaed300f25403fbd5650b2
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2026-08-21 18:49:55 +0600

    refactor Убрал лишний using

--- AutoCheckZapret/ViewModels/MainViewModel.cs
+++ AutoCheckZapret/ViewModels/MainViewModel.cs
@@ -4,7 +4,6 @@ using AutoCheckZapret.Services;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Reflection;
-using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Documents;
